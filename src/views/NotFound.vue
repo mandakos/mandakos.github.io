c077067e348eaf0c7bc52ae9f09924fcda928a9d
Manda Kosola <t6koma04@students.oamk.fi>
2019-08-07 14:59:48 +0300

    CSS styles and 404-page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <p>Not Found</p>
+  <div class="not-found content">
+    <h1>404</h1>
+    <p></p>
+    <p>Pahoittelut!</p>
+    <p>Tämä sivu tai painamasi linkki on jostain syystä rikki.</p>
+    <img src="https://media.giphy.com/media/qVVVfmHDMBZug/source.gif">
+    <p>Jos nyt olotila tuntuu ylläolevan linnun kaltaiselta, 
+      niin <a href="/">klikkaa tästä</a> (ajaudut etusivulle).</p>
   </div>
 </template>
 
